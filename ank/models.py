f4c0051fdf66a63de571e0079ac17f7b596720c4
Art.py <lekcjephp@gmail.com>
2012-03-15 17:01:24 +0000

    utworzono baze danych, dodalismy jeden model do modeli, w url wlaczylismy admina

--- ank/models.py
+++ ank/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Poll (models.Model):
+    question=models.CharField(max_length=500)
+    pubdate=models.DateTimeField("data publikacji")
+
+class Choice (models.Model):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
